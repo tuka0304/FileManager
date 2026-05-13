12566b0261da033a219f22b4c77e3a00db42ad7a
tuka0304 <phido0002@gmail.com>
2026-05-13 18:00:45 +0700

    Thêm script tự động tạo Admin

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', '123456')"
